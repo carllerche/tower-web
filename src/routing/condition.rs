a1f2299e217320501f2cd787b2a8cb01d60f27d2
Carl Lerche <me@carllerche.com>
2018-03-20 22:36:46 -0700

    Extract params

--- src/routing/condition.rs
+++ src/routing/condition.rs
@@ -9,7 +9,7 @@ pub struct Condition {
     method: Method,
 
     /// Path used to match the route
-    path: String,
+    path: Segments,
 }
 
 #[derive(Debug)]
@@ -22,6 +22,11 @@ pub struct Match<T> {
 }
 
 #[derive(Debug)]
+struct Segments {
+    segments: Vec<Segment>,
+}
+
+#[derive(Debug)]
 enum Segment {
     Literal(String),
     Param,
@@ -30,26 +35,25 @@ enum Segment {
 // ===== impl Condition =====
 
 impl Condition {
+    /// Create a new condition
     pub fn new(method: Method, path: &str) -> Condition {
-        let path = path.to_string();
+        let path = Segments::new(path);
 
         Condition {
             method,
-            path
+            path,
         }
     }
 
+    /// Test a request
     pub fn test(&self, request: &Request<()>) -> Option<Params> {
         if *request.method() != self.method {
             return None;
         }
 
-        if request.uri().path() != self.path {
-            return None;
-        }
-
-        unimplemented!();
+        self.path.test(request.uri().path())
     }
+
 }
 
 // ===== impl Match =====
@@ -78,4 +82,89 @@ impl<T> Match<T> {
     pub(crate) fn into_parts(self) -> (T, Params) {
         (self.destination, self.params)
     }
+}
+
+// ===== impl Segments =====
+
+impl Segments {
+    /// Create a new condition
+    pub fn new(mut path: &str) -> Segments {
+        if !path.is_empty() && &path[path.len()-1..path.len()] == "/" {
+            path = &path[0..path.len()-1];
+        }
+
+        let segments = path.split("/")
+            .map(|segment| {
+                if segment.chars().next() == Some(':') {
+                    Segment::Param
+                } else {
+                    Segment::Literal(segment.to_string())
+                }
+            })
+            .collect();
+
+        Segments { segments }
+    }
+
+    /// Test the path component of a request
+    fn test(&self, mut path: &str) -> Option<Params> {
+        if !path.is_empty() && &path[path.len()-1..path.len()] == "/" {
+            path = &path[0..path.len()-1];
+        }
+
+        let mut i = 0;
+        let mut params = vec![];
+
+        for segment in path.split("/") {
+            if i == self.segments.len() {
+                return None;
+            }
+
+            match self.segments[i] {
+                Segment::Param => params.push(segment.into()),
+                Segment::Literal(ref val) => {
+                    if segment != val {
+                        return None;
+                    }
+                }
+            }
+
+            i += 1;
+        }
+
+        if i != self.segments.len() {
+            return None;
+        }
+
+        Some(Params::new(params))
+    }
+}
+
+#[test]
+fn test_segments() {
+    let slash = Segments::new("/");
+
+    assert!(slash.test("/").is_some());
+    assert!(slash.test("/foo").is_none());
+    assert!(slash.test("//").is_none());
+
+    let one_lit = Segments::new("/foo");
+
+    assert!(one_lit.test("/foo").is_some());
+    assert!(one_lit.test("/").is_none());
+    assert!(one_lit.test("/bar").is_none());
+    assert!(one_lit.test("/foo/bar").is_none());
+
+    let capture = Segments::new("/:id");
+    let params = capture.test("/foo").unwrap();
+    assert_eq!(params.len(), 1);
+    assert_eq!(&params[0], "foo");
+
+    assert!(capture.test("/").is_none());
+
+    let multi = Segments::new("/:one/:two");
+    let params = multi.test("/foo/bar").unwrap();
+    assert_eq!(params.len(), 2);
+    assert_eq!(&params[0], "foo");
+    assert_eq!(&params[1], "bar");
 }
--- src/routing/params.rs
+++ src/routing/params.rs
@@ -1,16 +1,32 @@
-use bytes::Bytes;
+use std::ops;
 
 #[derive(Debug)]
 pub struct Params {
     /// Parameters extracted from the requet
     ///
     /// TODO: More efficient extraction.
-    params: Vec<Bytes>,
+    params: Vec<String>,
 }
 
 impl Params {
+    pub(crate) fn new(params: Vec<String>) -> Params {
+        Params { params }
+    }
+
+    pub fn len(&self) -> usize {
+        self.params.len()
+    }
+
     /// Get a parameter value
     pub fn get(&self, index: usize) -> &[u8] {
         self.params[index].as_ref()
+    }
+}
+
+impl ops::Index<usize> for Params {
+    type Output = str;
+
+    fn index(&self, index: usize) -> &str {
+        &self.params[index]
     }
 }
