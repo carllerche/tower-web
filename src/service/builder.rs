aa4128a82ad5ef6a9292d0cf6bdb07e6e4e51dcb
Carl Lerche <me@carllerche.com>
2018-07-30 23:15:54 -0700

    No more RequestBody on ServiceBuilder

--- src/service/builder.rs
+++ src/service/builder.rs
@@ -9,68 +9,56 @@ use std::net::SocketAddr;
 use std::sync::Arc;
 
 /// Builds a web service
-///
-/// TODO: Is `RequestBody` required here?
 #[derive(Debug)]
-pub struct ServiceBuilder<T, Middleware, RequestBody> {
+pub struct ServiceBuilder<T, Middleware> {
     /// The inner resource
     resource: T,
     middleware: Middleware,
-    _p: PhantomData<RequestBody>,
 }
 
-impl<RequestBody> ServiceBuilder<(), Identity, RequestBody> {
+impl ServiceBuilder<(), Identity> {
     /// Create a new `ServiceBuilder`
     pub fn new() -> Self {
         ServiceBuilder {
             resource: (),
             middleware: Identity::new(),
-            _p: PhantomData,
         }
     }
 }
 
-impl<T, Middleware, RequestBody> ServiceBuilder<T, Middleware, RequestBody> {
+impl<T, Middleware> ServiceBuilder<T, Middleware> {
     /// Add a resource handler.
     pub fn resource<U>(self, resource: U)
-        -> ServiceBuilder<<T as Chain<U>>::Output, Middleware, RequestBody>
+        -> ServiceBuilder<<T as Chain<U>>::Output, Middleware>
     where
         T: Chain<U>,
     {
         ServiceBuilder {
             resource: self.resource.chain(resource),
             middleware: self.middleware,
-            _p: PhantomData,
         }
     }
-}
 
-impl<T, Middleware, RequestBody> ServiceBuilder<T, Middleware, RequestBody>
-where
-    T: IntoResource<DefaultSerializer, RequestBody>,
-    // Middleware: Middleware<?>,
-    RequestBody: BufStream,
-{
     /// Build a service instance.
-    pub fn build(self) -> WebService<T::Resource, RequestBody> {
+    pub fn build<RequestBody>(self) -> WebService<T::Resource, RequestBody>
+    where T: IntoResource<DefaultSerializer, RequestBody>,
+          RequestBody: BufStream,
+    {
         let routes = Arc::new(self.resource.routes());
 
         WebService::new(
             self.resource.into_resource(DefaultSerializer::new()),
             routes)
     }
-}
 
-impl<T, M> ServiceBuilder<T, M, ::run::LiftReqBody>
-where
-    T: IntoResource<DefaultSerializer, ::run::LiftReqBody>,
-    T::Resource: Send + 'static,
-    <T::Resource as Resource>::Buf: Send,
-    <T::Resource as Resource>::Body: Send,
-    <T::Resource as Resource>::Future: Send,
-{
     /// Run the service
-    pub fn run(self, addr: &SocketAddr) -> io::Result<()> {
+    pub fn run(self, addr: &SocketAddr) -> io::Result<()>
+    where T: IntoResource<DefaultSerializer, ::run::LiftReqBody>,
+          T::Resource: Send + 'static,
+          <T::Resource as Resource>::Buf: Send,
+          <T::Resource as Resource>::Body: Send,
+          <T::Resource as Resource>::Future: Send,
+    {
         fn assert_send<T: Send>() {}
         assert_send::<::run::LiftReqBody>();
         assert_send::<DefaultSerializer>();
