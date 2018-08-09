c51526e796f4fc17dd4f241a9e0ea02b745b8181
Carl Lerche <me@carllerche.com>
2018-08-09 12:26:50 -0700

    Bump version to v0.1.1.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# 0.1.1 (August 9, 2018)
+
+* Allow warnings to make docs.rs happy.
+
 # 0.1.0 (August 9, 2018)
 
 * Initial release
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/tower-web/0.1.0")]
+#![doc(html_root_url = "https://docs.rs/tower-web/0.1.1")]
 #![deny(missing_debug_implementations)]
 #![cfg_attr(test, deny(warnings))]
 
