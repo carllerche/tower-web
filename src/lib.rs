3d19d88655f1d40505470181baa4b0e0b0454c12
Carl Lerche <me@carllerche.com>
2018-08-09 21:52:55 -0700

    Bump version to v0.1.2 (#58)

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/tower-web/0.1.1")]
+#![doc(html_root_url = "https://docs.rs/tower-web/0.1.2")]
 #![deny(missing_debug_implementations)]
 #![cfg_attr(test, deny(warnings))]
 
