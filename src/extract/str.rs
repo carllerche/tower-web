298c26d015f48009f6e1440c94115ceb129764e1
Laurentiu Nicola <lnicola@dend.ro>
2018-09-09 11:38:22 +0300

    extract: percent-decode Strings

--- src/extract/str.rs
+++ src/extract/str.rs
@@ -1,5 +1,12 @@
-use extract::{Extract, Error, Context, Immediate};
+use extract::{Context, Error, Extract, Immediate};
+use std::borrow::Cow;
+use url::percent_encoding;
 use util::BufStream;
+
+fn decode(s: &str) -> Result<String, Error> {
+    let percent_decoded = Cow::from(percent_encoding::percent_decode(s.as_bytes())).into_owned();
+    String::from_utf8(percent_decoded).map_err(|e| Error::invalid_argument(&e))
+}
 
 impl<B: BufStream> Extract<B> for String {
     type Future = Immediate<String>;
@@ -10,10 +17,9 @@ impl<B: BufStream> Extract<B> for String {
         match ctx.callsite().source() {
             Capture(idx) => {
                 let path = ctx.request().uri().path();
-                let value = ctx.captures().get(*idx, path)
-                    .to_string();
+                let value = ctx.captures().get(*idx, path);
 
-                Immediate::ok(value)
+                Immediate::result(decode(value))
             }
             Header(header_name) => {
                 let value = match ctx.request().headers().get(header_name) {
@@ -23,18 +29,16 @@ impl<B: BufStream> Extract<B> for String {
                     }
                 };
 
-                match value.to_str() {
-                    Ok(s) => Immediate::ok(s.to_string()),
-                    Err(_) => Immediate::err(Error::invalid_argument(&"invalid UTF-8 string")),
-                }
+                let r = value
+                    .to_str()
+                    .map(|s| s.to_string())
+                    .map_err(|e| Error::invalid_argument(&e));
+                Immediate::result(r)
             }
             QueryString => {
-                let query = ctx.request().uri()
-                    .path_and_query()
-                    .and_then(|path_and_query| path_and_query.query())
-                    .unwrap_or("");
+                let query = ctx.request().uri().query().unwrap_or("");
 
-                Immediate::ok(query.to_string())
+                Immediate::result(decode(query))
             }
             Body => {
                 unimplemented!();
@@ -43,5 +47,16 @@ impl<B: BufStream> Extract<B> for String {
                 unimplemented!();
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn extract() {
+        assert_eq!("hello, world", decode("hello,%20world").unwrap());
+        assert!(decode("%ff").unwrap_err().is_invalid_argument());
     }
 }
