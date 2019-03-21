f86a7259459954171827a6ebfd86e994cc228bf9
Abdel-Rahman A <abogical@gmail.com>
2019-03-22 01:25:46 +0200

    Prevent duplicate routes in compile time (#195)

---
+++ tower-web-macros/src/resource/test/parse.rs
@@ -0,0 +1,22 @@
+
+macro_rules! expand {
+    ($($tt:tt)*) => {{
+        ::resource::expand_derive_resource(quote!($($tt)*))
+    }}
+}
+
+#[test]
+#[should_panic(expected = "duplicate routes with method")]
+fn duplicate_routes(){
+    expand! {
+        impl Test{
+            #[get("/foo")]
+            #[content_type("plain")]
+            fn foo(&self){}
+
+            #[get("/foo")]
+            #[content_type("plain")]
+            fn bar(&self){}
+        }
+    };
+}
